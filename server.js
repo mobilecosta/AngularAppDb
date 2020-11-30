11234dfe001b51062b7b505577e39c66236b0a2a
mobile.costa@gmail.com <mobile.costa@gmail.com>
2020-11-29 22:58:06 -0300

    Versão Inicial

---
+++ server.js
@@ -0,0 +1,30 @@
+const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+// Customiza retorno do metodo Get
+router.render = (req, res) => {
+  const data = res.locals.data;
+  if (data && data instanceof Array) {
+    res.jsonp({
+        items: data,
+        hasNext: false
+      })
+  } else {
+    res.jsonp(data)
+  }
+}
+
+server.db = router.db
+
+// Use default router
+server.use(auth)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
